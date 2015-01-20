7e4653464235dd2a501d74347bef2b28e76c6368
heyjones <chris@heyjones.com>
2015-01-19 22:21:07 -0800

    initial commit:

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.4'
+
+gem 'uglifier', '>= 1.3.0'
+gem 'sass-rails', '~> 4.0.3'
+gem 'jquery-rails'
+
+group :development do
+	gem 'sqlite3'
+	gem 'spring'
+end
+
+group :production do
+	gem 'pg'
+	gem 'unicorn'
+end
+
+gem 'shopify_app'
+
+group :development, :test do
+  gem "less-rails-bootstrap"
+  gem "therubyracer", platforms: :ruby
+end
---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Marketplace
+  class Application < Rails::Application
+
+    # Shopify API connection credentials:
+    config.shopify.api_key = '36a456ae13abeb44494eb1f1b3973968'
+    config.shopify.secret = 'c89772eccfe51a7917c7e9d0a141b3bb'
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+  end
+end
